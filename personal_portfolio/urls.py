a782387053337c78f67a2bc5d10475c8cfc9d1c6
terramart <terramart2020@gmail.com>
2021-01-10 13:07:43 +0800

    Updated home page

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,6 +22,8 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
     path('blog/', include('blog.urls')),
 ]
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,3 +4,9 @@ from .models import Project
 def home(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'projects':projects})
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')  
