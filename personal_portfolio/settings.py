a60c5edc370d4d50e0692b4e144a8ffb11832cbc
terramart <terramart2020@gmail.com>
2021-01-10 23:53:19 +0800

    Update on settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tht(vp-c1l7op9d+jcu9)_e$yc054f*h*5@!uu8(!%eulc*u)y'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -129,3 +132,6 @@ try:
     from .local_settings import *
 except ImportError:
     print('Looks like no local file. You must be on production')
+
+
+
